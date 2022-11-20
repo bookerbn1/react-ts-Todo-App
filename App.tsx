9eaf8035faa47157b433386e8967f7c11d17a50d
bookerbn1 <76966996+bookerbn1@users.noreply.github.com>
2022-11-19 23:30:14 -0500

    1 file modified

--- App.tsx
+++ App.tsx
@@ -16,7 +16,6 @@ export default function App() {
   // tasks state
   const [toDo, setToDo] = useState([
     {
-      id: 1,
       Title: 'Task 1',
       Description: 'Description of Task 1',
       Deadline: '01/01/2022',
@@ -25,7 +24,6 @@ export default function App() {
       IsComplete: false,
     },
     {
-      id: 2,
       Title: 'Task 2',
       Description: 'Description of Task 1',
       Deadline: '01/01/2022',
@@ -34,50 +32,29 @@ export default function App() {
     },
   ]);
 
-  // Temp States
+  // Temp States for entry
   const [newTitle, setNewTitle] = useState();
   const [newDeadline, setNewDeadline] = useState();
   const [newDescription, setNewDescription] = useState();
   const [newPriority, setNewPriority] = useState();
 
+  // temp state for popup form
   const [buttonPopup, setButtonPopup] = useState(false);
-  const [buttonPopupUpdate, setButtonPopupUpdate] = useState(false);
-
-  // function to add task
-  const addTask = () => {
-    setButtonPopup(false);
-    if (newTitle) {
-      // Calling toast method by passing string
-      toast.success('Task was added succesfully');
-      let num = toDo.length + 1;
-      let newEntry = {
-        id: num,
-        Title: newTitle,
-        Description: newDescription,
-        Deadline: newDeadline,
-        Priority: newPriority,
-        IsComplete: false,
-      };
-      setToDo([...toDo, newEntry]);
-      setNewTitle();
-      setNewDeadline();
-      setNewDescription();
-      setNewPriority();
-    }
-  };
+  const [windowText, setWindowText] = useState();
+  const [windowIcon, setWindowIcon] = useState();
 
   // function to delete task
-  const deleteTask = (id) => {
-    let newTasks = toDo.filter((task) => task.id !== id);
+  const deleteTask = (newTitle) => {
+    let newTasks = toDo.filter((task) => task.Title !== newTitle);
     setToDo(newTasks);
     // Calling toast method by passing string
     toast.error('Deleted Task');
   };
 
   // function to mark task as done
-  const markDone = (id) => {
+  const markDone = (newTitle) => {
     let newTask = toDo.map((task) => {
-      if (task.id === id) {
+      if (task.Title === newTitle) {
         return { ...task, IsComplete: !task.IsComplete };
       }
       return task;
@@ -86,29 +63,27 @@ export default function App() {
     toast.success('Successfully completed a task!!');
   };
 
-  // prepopulate form with data to be updated
-  const editTask = (id, Title, Description, Deadline, Priority) => {
+  // function when the update button is clicked
+  const editTask = (Title, Description, Deadline, Priority) => {
+    setWindowText('Edit');
     setNewTitle(Title);
     setNewDescription(Description);
     setNewDeadline(Deadline);
     setNewPriority(Priority);
-    setButtonPopupUpdate(true);
-    toast.warning(Title);
+    setButtonPopup(true);
+    toast.warning('Updating' + ' ' + Title);
+    setWindowIcon(<FontAwesomeIcon icon={faPen} />);
   };
 
-  // update task
-  const updateTask = () => {
-    setButtonPopupUpdate(false);
-    setNewTitle();
-    setNewDeadline();
-    setNewDescription();
-    setNewPriority();
-    toast.success('Successfully Updated a task');
+  // function for when the add button is clicked
+  const PopupAdd = () => {
+    setWindowText('Add');
+    setButtonPopup(true);
+    setWindowIcon(<FontAwesomeIcon icon={faCirclePlus} />);
   };
 
   // function to cancel
   const cancelPopup = (e) => {
-    setButtonPopupUpdate(false);
     setButtonPopup(false);
     setNewTitle();
     setNewDeadline();
@@ -127,6 +102,43 @@ export default function App() {
     );
   };
 
+  // function to add task
+  const addTask = () => {
+    setButtonPopup(false);
+    if (!toDo.some((toDo) => toDo.Title == newTitle)) {
+      // Calling toast method by passing string
+      toast.success('Task was added succesfully');
+      let num = toDo.length + 1;
+      let newEntry = {
+        Title: newTitle,
+        Description: newDescription,
+        Deadline: newDeadline,
+        Priority: newPriority,
+        IsComplete: false,
+      };
+      setToDo([...toDo, newEntry]);
+    } else {
+      let newTask = toDo.map((task) => {
+        if (task.Title === newTitle) {
+          return {
+            ...task,
+            Description: newDescription,
+            Deadline: newDeadline,
+            Priority: newPriority,
+            IsComplete: false,
+          };
+        }
+        return task;
+      });
+      setToDo(newTask);
+      toast.warning('Successfully Updated a task');
+    }
+    setNewTitle();
+    setNewDeadline();
+    setNewDescription();
+    setNewPriority();
+  };
+
   return (
     <div className="container App">
       <br />
@@ -135,10 +147,7 @@ export default function App() {
         FRAMEWORKS
         {/* add task */}
         <span className="addButton">
-          <button
-            onClick={() => setButtonPopup(true)}
-            class="btn btn-md btn-primary"
-          >
+          <button onClick={() => PopupAdd()} class="btn btn-md btn-primary">
             <FontAwesomeIcon icon={faCirclePlus} /> Add
           </button>
         </span>
@@ -164,7 +173,7 @@ export default function App() {
           .sort((a, b) => (a.Deadline > b.Deadline ? 1 : -1))
           .map((task, index) => {
             return (
-              <React.Fragment key={task.id}>
+              <React.Fragment key={task.Title}>
                 <div className="col taskBg">
                   <table>
                     <th></th>
@@ -176,14 +185,14 @@ export default function App() {
                       <td>
                         <input
                           type="checkbox"
-                          onChange={(e) => markDone(task.id)}
+                          onChange={(e) => markDone(task.Title)}
                         />
                       </td>
                       <td>
                         <div className="deleteButton">
                           <span
                             title="Delete"
-                            onClick={() => deleteTask(task.id)}
+                            onClick={() => deleteTask(task.Title)}
                           >
                             <button className="btn btn-sm btn-danger">
                               <FontAwesomeIcon icon={faCircleXmark} /> DELETE
@@ -197,7 +206,6 @@ export default function App() {
                                 title="Edit"
                                 onClick={() =>
                                   editTask(
-                                    task.id,
                                     task.Title,
                                     task.Description,
                                     task.Deadline,
@@ -226,7 +234,8 @@ export default function App() {
                 <Popup trigger={buttonPopup}>
                   <form onSubmit={(e) => addTask()}>
                     <div class="card-header text-white bg-primary">
-                      <FontAwesomeIcon icon={faCirclePlus} /> Add Task
+                      {windowIcon}
+                      {windowText} Task
                     </div>
                     <div class="card-body">
                       <div class="form-group row">
@@ -336,135 +345,8 @@ export default function App() {
                                 type="submit"
                                 className="btn btn-primary btn-md"
                               >
-                                <FontAwesomeIcon icon={faCirclePlus} /> Add
-                              </button>
-                              <button
-                                className="btn btn-danger btn-md"
-                                onClick={(e) => cancelPopup(e)}
-                              >
-                                <FontAwesomeIcon icon={faCircleXmark} /> Cancel
-                              </button>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </form>
-                </Popup>
-
-                {/* Update Task */}
-                {/* Buttonpop up trigger */}
-                <Popup trigger={buttonPopupUpdate}>
-                  <form onsubmit={(e) => updateTask()}>
-                    <div class="card-header text-white bg-primary">
-                      <FontAwesomeIcon icon={faCirclePlus} /> Edit Task
-                    </div>
-                    <div class="card-body">
-                      <div class="form-group row">
-                        <div class="col">
-                          <input
-                            id="Title"
-                            name="Title"
-                            type="text"
-                            value={newTitle}
-                            onBlur={(e) => setNewTitle(e.target.value)}
-                            required="required"
-                            class="form-control"
-                            disabled
-                          />
-                        </div>
-                      </div>
-                      <br></br>
-                      <div class="form-group row">
-                        <div class="col">
-                          <input
-                            id="Description"
-                            name="Description"
-                            type="text"
-                            value={newDescription}
-                            onBlur={(e) => setNewDescription(e.target.value)}
-                            class="form-control"
-                            required="required"
-                          />
-                        </div>
-                      </div>
-                      <br></br>
-                      <div class="row">
-                        <div class="col">
-                          <div class="form-date">
-                            <br />
-                            <input
-                              id="Deadline"
-                              type="date"
-                              value={newDeadline}
-                              onChange={(e) => setNewDeadline(e.target.value)}
-                              class="form-control"
-                              required="required"
-                            />
-                          </div>
-                        </div>
-                      </div>
-                      <br></br>
-                      <div class="form-group">
-                        <label class="form-label">Priority:</label>
-                        <div class="form-group row">
-                          <div class="custom-control custom-radio custom-control-inline">
-                            <input
-                              name="Priority"
-                              id="Priority_0"
-                              type="radio"
-                              class="custom-control-input"
-                              value="low"
-                              required="required"
-                            />
-                            <label
-                              for="Priority_0"
-                              class="custom-control-label"
-                            >
-                              Low
-                            </label>
-                          </div>
-                          <div class="custom-control custom-radio custom-control-inline">
-                            <input
-                              name="Priority"
-                              id="Priority_1"
-                              type="radio"
-                              class="custom-control-input"
-                              value="med"
-                              required="required"
-                            />
-                            <label
-                              for="Priority_1"
-                              class="custom-control-label"
-                            >
-                              Med
-                            </label>
-                          </div>
-                          <div class="custom-control custom-radio custom-control-inline">
-                            <input
-                              name="Priority"
-                              id="Priority_2"
-                              type="radio"
-                              class="custom-control-input"
-                              value="high"
-                              required="required"
-                            />
-                            <label
-                              for="Priority_2"
-                              class="custom-control-label"
-                            >
-                              High
-                            </label>
-                          </div>
-                          <hr></hr>
-                          <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                            <div class="d-grid d-md-flex-content-between">
-                              <button
-                                type="submit"
-                                className="btn btn-primary btn-md"
-                                onClick={(e, id) => updateTask(id)}
-                              >
-                                <FontAwesomeIcon icon={faCirclePlus} /> Update
+                                {windowIcon}
+                                {windowText}
                               </button>
                               <button
                                 className="btn btn-danger btn-md"
